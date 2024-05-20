03eb5c27e1517f25018f51285f84d7311938380b
Pparang <sojung0530@naver.com>
2024-05-20 15:43:16 +0900

    Reply 기능 구동에 필요한 임시 매퍼/컨트롤러/서비스/VO 추가

---
+++ mvplay_on/src/main/java/com/mvp/mapper/ReplyMapper.java
@@ -0,0 +1,23 @@
+package com.mvp.mapper;
+
+import java.util.List;
+
+import com.mvp.model.ReplyVO;
+
+public interface ReplyMapper {
+
+	//댓글 조회
+	public List<ReplyVO> readReply(int bno);
+	
+	//댓글 작성
+	public void writeReply(ReplyVO vo);
+	
+	//특정 댓글 조회
+	public ReplyVO readReplySelect(int rno);
+	
+	//댓글 수정
+	public void replyUpdate(ReplyVO vo);
+	
+	//댓글 삭제
+	public void replyDelete(ReplyVO vo);
+}
---
+++ mvplay_on/src/main/java/com/mvp/model/ReplyVO.java
@@ -0,0 +1,70 @@
+package com.mvp.model;
+
+import java.util.Date;
+
+public class ReplyVO {
+	
+	//댓글 번호
+	private int replyNum;
+	
+	//유저 ID
+	private String userId;
+	
+	//영화 ID
+	private int movieId;
+	
+	//내용
+	private String replyContent;
+	
+	//작성 일자
+	private Date regDate;
+
+	
+	public int getReplyNum() {
+		return replyNum;
+	}
+
+	public void setReplyNum(int replyNum) {
+		this.replyNum = replyNum;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getReplyContent() {
+		return replyContent;
+	}
+
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	@Override
+	public String toString() {
+		return "ReplyVO [replyNum=" + replyNum + ", userId=" + userId + ", movieId=" + movieId + ", replyContent="
+				+ replyContent + ", regDate=" + regDate + "]";
+	}
+	
+	
+}
