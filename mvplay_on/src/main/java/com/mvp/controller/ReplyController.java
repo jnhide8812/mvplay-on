1f9fc15a43a0fbb76ca747b14c0256bd3fef3551
Pparang <sojung0530@naver.com>
2024-05-20 15:51:00 +0900

    ReplyController 수정

--- mvplay_on/src/main/java/com/mvp/controller/ReplyController.java
+++ mvplay_on/src/main/java/com/mvp/controller/ReplyController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.mvp.model.ReplyVO;
+import com.mvp.model.ReplyVO; 
 import com.mvp.service.ReplyService;
 
 public class ReplyController {
